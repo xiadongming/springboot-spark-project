861259d1cf54fad186eded5989b7ddd673121768
xiadongming <1816808381@qq.com>
2021-02-27 17:24:01 +0800

    UUID测试

---
+++ springboot-spark-project2/src/main/java/com/itchina/test/UUIDTest.java
@@ -0,0 +1,18 @@
+package com.itchina.test;
+
+import java.util.UUID;
+
+/**
+ * @Date: 2021/2/27 15:59
+ * @Desc:
+ */
+public class UUIDTest {
+
+    public static void main(String[] args) {
+
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid.toString());
+
+    }
+
+}
