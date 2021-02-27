9ab568ba0ccfd7c68fa1575ec4b47e2e12d26685
xiadongming <1816808381@qq.com>
2021-02-27 21:51:01 +0800

    定义jwt的拦截器，同一处理jwt

--- springboot-spark-project2/src/main/java/com/itchina/session/TokenTest.java
+++ springboot-spark-project2/src/main/java/com/itchina/session/TokenTest.java
@@ -4,10 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
@@ -18,15 +15,22 @@ import java.util.Date;
  * @Desc:  jwt即token的登录和验证流程
  */
 @RestController
-@RequestMapping("/dev")
+@RequestMapping("/devJwt")
 public class TokenTest {
+
+    @RequestMapping(value = "/address", method = {RequestMethod.GET, RequestMethod.POST})
+    public String getLoginWithIntercepter(@RequestAttribute String userIdFromHandle) {
+
+        return userIdFromHandle;
+    }
+
 
     @RequestMapping(value = "/jwtLogin", method = {RequestMethod.GET, RequestMethod.POST})
     public String getLogin(String userName, String pasWord, HttpServletResponse response) {
         String token = null;
         try {
             Algorithm algorithm = Algorithm.HMAC256("miyao");
-            token = JWT.create().withIssuer("managerRole").withExpiresAt(new Date(System.currentTimeMillis() + 3600000)).sign(algorithm);
+            token = JWT.create().withClaim("id","Claim_liudehua").withExpiresAt(new Date(System.currentTimeMillis() + 3600000)).sign(algorithm);
             //1h过期时间
 
             response.setHeader("token", token);
