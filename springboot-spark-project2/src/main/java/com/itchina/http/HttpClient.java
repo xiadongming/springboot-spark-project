b4c1d0978ad74cf6737340394ba91945aede0996
xiadongming <dongmingxia2016@163.com>
2021-03-10 12:56:31 +0800

    http请求和转发

---
+++ springboot-spark-project2/src/main/java/com/itchina/http/HttpClient.java
@@ -0,0 +1,9 @@
+package com.itchina.http;
+
+/**
+ * @Date: 2021/3/10 12:55
+ * @Desc:  1-发送http请求
+ *         2-转发http请求
+ */
+public class HttpClient {
+}
---
+++ springboot-spark-project2/src/main/java/com/itchina/test/Test4.java
@@ -0,0 +1,33 @@
+package com.itchina.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Date: 2021/3/10 10:31
+ * @Desc:
+ */
+public class Test4 {
+
+    public static void main(String[] args) {
+        Integer abc = 2;
+        Integer abcd = new Integer(2);
+        /*String IS_SELF = "统一供货厅&!外部合作厅&!移动合作厅&!终端自营厅&!专业公司合作厅";
+        String[] split = IS_SELF.split("&!");
+        List<String> stringList = Arrays.asList(split);
+
+        System.out.println(stringList.get(0));
+        System.out.println(stringList.get(1));
+        System.out.println(stringList.get(2));
+*/
+
+
+        abcs(abcd);
+
+    }
+
+    private static void abcs(Integer abc) {
+        System.out.println(null == abc);
+
+    }
+}
