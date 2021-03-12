a10e5c669c1db985a8e424acdf04522ed12f81eb
xiadongming <dongmingxia2016@163.com>
2021-03-12 18:46:03 +0800

    添加用户基类和从security中获取数据

---
+++ springboot-spark-project2/src/main/java/com/itchina/common/UserInfoBaseBO.java
@@ -0,0 +1,9 @@
+package com.itchina.common;
+
+/**
+ * @Date: 2021/3/12 18:44
+ * @Desc: 定义用户基类，继承这个类即可获取用户信息
+ *        待实现
+ */
+public class UserInfoBaseBO {
+}
---
+++ springboot-spark-project2/src/main/java/com/itchina/security/SecurityHelper.java
@@ -0,0 +1,9 @@
+package com.itchina.security;
+
+/**
+ * @Date: 2021/3/12 18:43
+ * @Desc: 从security中获取用户信息
+ *        待实现
+ */
+public class SecurityHelper {
+}
