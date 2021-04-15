06b4b7da8cc1c93bc37f2c039ee4097e56ccfb52
xiadongming <1816808381@qq.com>
2021-04-16 07:52:46 +0800

    自定义锁，可重入和不可重入锁

--- springboot-spark-project2/src/main/java/com/itchina/lock/MyLock2.java
+++ springboot-spark-project2/src/main/java/com/itchina/lock/MyLock2.java
@@ -6,22 +6,14 @@ import java.util.concurrent.locks.Lock;
 
 /**
  * @Date: 2021/4/15 22:27
- * @Desc: 实现自定义锁    可重入性
+ * @Desc: 实现自定义锁    不可重入性
  */
-public class MyLock implements Lock {
+public class MyLock2 implements Lock {
     /**
      * 是否获取锁，锁标志位
      */
     private boolean isLocked = false;
 
-    /**
-     * 通过判断锁的所有者，实现锁的可重入性
-     */
-    private Thread owner = null;
-    /**
-     * 可重入的计数器
-     */
-    private int lockCount = 0;
 
     @Override
     public synchronized void lock() {
@@ -32,7 +24,7 @@ public class MyLock implements Lock {
          * 2-可重入性
          * */
         Thread currentThread = Thread.currentThread();
-        while (isLocked || currentThread != owner) {
+        while (isLocked) {
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -40,24 +32,16 @@ public class MyLock implements Lock {
             }
         }
         isLocked = true;
-        owner = currentThread;
-        lockCount++;
     }
 
 
     @Override
     public synchronized void unlock() {
-        Thread currentThread = Thread.currentThread();
-        if (currentThread == owner) {
-            lockCount -- ;
-            if(lockCount == 0){
-                isLocked = false;
-                /**
-                 * 将等待中的线程唤醒
-                 * */
-                notify();
-            }
-        }
+        isLocked = false;
+        /**
+         * 将等待中的线程唤醒
+         * */
+        notify();
     }
 
     /**
